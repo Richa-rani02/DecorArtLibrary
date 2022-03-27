3c7ff2011ef70701e9dd4de4062fe5deec98e8a7
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-03-28 01:59:09 +0530

    changed colors and added navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import {Routes,Route} from "react-router-dom";
 import Mockman from "mockman-js";
 import {Home,Explore} from "./Pages/index";
+import {Header} from "./components/index";
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/explore" element={<Explore/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import "./Header.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {Link} from "react-router-dom";
 export const Header = () => {
     
     return (
         <header>
+            <div className="left_area">
+        <label for="check">
+          <GiHamburgerMenu className="hamburger-icon" />
+        </label>
+        <div>
+          <h1>Decor Art</h1>
+        </div>
+      </div>
+      <form action="" className="search-form" >
+                <input type="search" id="search-box" placeholder="search here..."
+                    value="" />
+                <label htmlFor="search-box" className="fas fa-search"></label>
+            </form>
+      <div className="right_area">
+      <Link to="/" className="btn btn-solid-primary btn-sm btn-rounded-5">Login</Link>
+      </div>
         </header>
     )
 }
