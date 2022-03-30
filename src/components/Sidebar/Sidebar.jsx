d4abccbf73e51e818ba9e728ccb619f8d6f8e55d
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-03-30 14:36:24 +0530

    feat:added links to sidebar and made it responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,35 +1,39 @@
 import {Link} from "react-router-dom";
 import {ImHome} from "react-icons/im";
+import {RiPlayListAddFill,RiHistoryLine} from "react-icons/ri";
+import {MdExplore} from "react-icons/md";
+import {AiOutlineClockCircle} from "react-icons/ai";
+import "./Sidebar.css";
 export const Sidebar=()=>{
     return(
-        <ul className="nav-lists ">
+        <ul className="nav-lists list-style-none">
             <li>
                 <Link to="">
-                <ImHome/>
+                <ImHome className="icon-size"/>
                <span className="links_name">Home</span>
                 </Link>
             </li>
             <li>
                 <Link to="">
-                <ImHome/>
+                <MdExplore className="icon-size"/>
                <span className="links_name">Explore</span>
                 </Link>
             </li>
             <li>
                 <Link to="">
-                <ImHome/>
+                <RiPlayListAddFill className="icon-size"/>
                <span className="links_name">Playlist</span>
                 </Link>
             </li>
             <li>
                 <Link to="">
-                <ImHome/>
+                <RiHistoryLine className="icon-size"/>
                <span className="links_name">History</span>
                 </Link>
             </li>
             <li>
                 <Link to="">
-                <ImHome/>
+                <AiOutlineClockCircle className="icon-size"/>
                <span className="links_name">Watch Later</span>
                 </Link>
             </li>
