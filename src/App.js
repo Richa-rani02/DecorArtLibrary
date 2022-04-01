a940f42ddc99cce46ff96f1799e4339c5ca6a5af
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-01 08:45:03 +0530

    added css for video page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import {Routes,Route} from "react-router-dom";
 import Mockman from "mockman-js";
-import {Home,Explore,Login,WatchLater} from "./Pages/index";
+import {Home,Explore,Login,WatchLater,VideoPage} from "./Pages/index";
 import {Header} from "./components/index";
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
         <Route path="/login" element={<Login />}/>
         <Route path="/mock" element={<Mockman />}/>
         <Route path="/watchlater" element={<WatchLater/>}/>
+        <Route path="/videos" element={<VideoPage/>}/>
+
       </Routes>
     </div>
   );
