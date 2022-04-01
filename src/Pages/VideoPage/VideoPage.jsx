c1d0456eddd169aff3d877c9cc0c46585bfdf691
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-01 09:17:15 +0530

    added iframe

--- src/Pages/VideoPage/VideoPage.jsx
+++ src/Pages/VideoPage/VideoPage.jsx
@@ -1,14 +1,17 @@
 import "./VideoPage.css"; 
-
+import { SingleVideo } from "../../components/SingleVideo/SingleVideo";
 export const VideoPage=()=>{
     return(
         <div className="videopage">
          <div className="video_note_panel">
           <div className="video_panel">
-
+           <SingleVideo/>
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quod incidunt, cum ad necessitatibus dolorem nam numquam enim autem expedita assumenda sed ratione dolor? Mollitia, hic. Placeat quam possimus laudantium. */}
+            
               </div> 
               <div className="note_panel">
               
+              {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eligendi assumenda placeat, necessitatibus explicabo voluptatum corporis laborum atque, at veniam facere suscipit cum?</p> */}
               </div>   
 
          </div>
