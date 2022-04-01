8499e851a0a8399300714ea6093ecd72efa6ced4
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-01 08:31:01 +0530

    added css for videopage

---
+++ src/Pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,18 @@
+import "./VideoPage.css"; 
+
+export const VideoPage=()=>{
+    return(
+        <div className="videopage">
+         <div className="video_note_panel">
+          <div className="video_panel">
+
+              </div> 
+              <div className="note_panel">
+              
+              </div>   
+
+         </div>
+
+        </div>
+    )
+}
