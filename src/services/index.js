52c79b7d73ad6a507db788d96676ff8132e89a07
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-02 21:33:03 +0530

    fix:imported services to index

--- src/services/index.js
+++ src/services/index.js
@@ -3,4 +3,4 @@ export {getVideos} from "./videoServices";
 export {userLogin} from "./authServices";
 export {addToWatchLater,removeFromWatchLater} from "./watchLaterServices";
 export {addToHistory,removeFromHistory} from "./historyServices";
-export {addToPlaylist} from "./playlistServices";
+export {addToPlaylist,addVideosToPlaylist} from "./playlistServices";
