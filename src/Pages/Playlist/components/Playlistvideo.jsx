c01beeb48c98bffe912e4fa309ef2df6ac77a095
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-04 09:51:36 +0530

    fix:bug fixes

--- src/Pages/Playlist/components/Playlistvideo.jsx
+++ src/Pages/Playlist/components/Playlistvideo.jsx
@@ -6,24 +6,23 @@ export const Playlistvideo = () => {
     const { playlistId } = useParams();
     const { state: { playlists } } = useStateContext();
     const playlistList = playlists?.find((play) => play._id === playlistId);
-    const { title, videos } = playlistList;
-    return videos.length > 0 ? (
+    return playlistList? (
         <>
             <Sidebar />
             <div className="category-video playlistvideo-container">
                 <div className="videoimg_list_wrap">
                     <div className="video_img_wrap">
                         <div className="playlistimg-container bottom-gutter-sm">
-                            {videos.slice(0, 1).map(video => (
+                            {playlistList.videos.slice(0, 1).map(video => (
                                 <img src={`https://img.youtube.com/vi/${video._id}/0.jpg`} alt="video" className="img-responsive" />
                             ))}
                         </div>
-                        <span>{title}</span>
+                        <span>{playlistList.title}</span>
 
                     </div>
                     <div className="videolist_wrap">
                         {
-                            videos.map(clip => (
+                            playlistList.videos.map(clip => (
                                 < HorizontalCard key={clip._id} videos={clip} listId={playlistId} />
                             ))
                         }
--- src/components/HorizontalCard/HorizontalCard.jsx
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -1,16 +1,24 @@
 import "./HorizontalCard.css";
 import { MdOutlineDelete } from "react-icons/md";
-import {removeVideosFromPlaylist } from "../../services/index";
+import {removeVideosFromPlaylist,addToHistory } from "../../services/index";
 import { useStateContext } from "../../context/state-context";
 import { useAuth } from "../../context/auth-context";
+import { isInList } from "../videoCard/helper";
+import { useNavigate } from "react-router-dom";
 export const HorizontalCard=({videos,listId})=>{
-const {dispatch}=useStateContext();
-const {authState:{token}}=useAuth();
 
+const {state:{history},dispatch}=useStateContext();
+const {authState:{token}}=useAuth();
+const navigate = useNavigate();
+const isInHistory = isInList(history,videos._id);
+const videoClickHandler = () => {
+    navigate(`/video/${videos._id}`);
+    token && !isInHistory && addToHistory(dispatch, token, videos);
+}
     return(
         <>
        <div className="video-horizontal-card">
-          <div className="horizontal-img">
+          <div className="horizontal-img" onClick={() => videoClickHandler()} >
           <img src={`https://img.youtube.com/vi/${videos._id}/0.jpg`} alt="video" className="img-responsive" />
           </div>
           <h4 className="video-title title-width">{videos.title}</h4>
