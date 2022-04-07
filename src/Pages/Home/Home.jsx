9c1b436cade3fd20c0f26bcefa525349dd83a498
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-07 14:02:07 +0530

    feat:added fav icon

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home=()=>{
         <h4 className="h1">Want to learn how to make DIY Decor Items for your home ?</h4>
         <p className="center-text top-gutter-sm">Learn from best</p>
         <div className="home-btn-container flex flex-center">
-        <Link to="/explore" className="btn btn-solid-primary btn-lg btn-rounded-5 btn-color">Explore</Link>
-       <Link to="/login" className="btn btn-outline-primary btn-lg btn-rounded-5">Login</Link>
+        <Link to="/explore" className="btn btn-solid-primary btn-sm btn-rounded-5 btn-color">Explore</Link>
+       <Link to="/login" className="btn btn-outline-primary btn-sm btn-rounded-5">Login</Link>
        </div>
         </div>
         </section>
