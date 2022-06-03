fac5aa792bd199748498bcdbdcc6d27ef3331552
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-03 23:45:42 +0530

    feat:loader

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,16 +1,17 @@
 import "./Explore.css";
 import { CategoryChip, VideoCard, Sidebar } from "../../components";
 import { useStateContext } from "../../context/state-context";
-import { searchVideos, sortVideos,sortVideosByDates } from "./helper";
+import { searchVideos, sortVideos, sortVideosByDates } from "./helper";
 import { useLocation } from "react-router-dom";
 import { dataActions } from "../../Utils/actions";
+import { ThreeDots } from "react-loader-spinner";
 import { Empty } from "../index";
 const Explore = () => {
-    const { state: { videos, searchVideo, filterByCategory,sortby }, dispatch } = useStateContext();
+    const { state: { isLoading,videos, searchVideo, filterByCategory, sortby }, dispatch } = useStateContext();
     const location = useLocation();
     const searchbyName = searchVideos(videos, searchVideo);
     const sortByCategory = sortVideos(searchbyName, filterByCategory);
-    const sortByDates=sortVideosByDates(sortByCategory,sortby);
+    const sortByDates = sortVideosByDates(sortByCategory, sortby);
     return (
         <>
             <Sidebar />
@@ -18,14 +19,18 @@ const Explore = () => {
                 <div className="videolist_panel bottom-gutter-md">
                     <CategoryChip />
                     <section className="filter-section">
-                        <select name="sortbydate" value={sortby} onChange={(e)=>dispatch({ type: dataActions.SORT_BY, payload: e.target.value })}>
+                        <select name="sortbydate" value={sortby} onChange={(e) => dispatch({ type: dataActions.SORT_BY, payload: e.target.value })}>
                             <option>Latest</option>
                             <option>Oldest</option>
                         </select>
                         <input className="search-box" type="search" placeholder="search videos....." value={searchVideo} onChange={(e) => dispatch({ type: dataActions.SEARCH, payload: e.target.value })}>
                         </input>
                     </section>
-                    {sortByDates.length > 0 ?
+                    {isLoading ? (
+                        <div className="loader">
+                            <ThreeDots color="#f86da0" height={80} width={80} />
+                        </div>
+                    ) : sortByDates.length > 0 ?
                         <section className="category-video-explore">
                             <div className="category-video-container-explore">
                                 {sortByDates.map((clip) => (
