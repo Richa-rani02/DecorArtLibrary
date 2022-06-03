0fe4f3b29625bb0f4aa99c0f0e601640777f3964
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-03 09:57:03 +0530

    refactor:home page UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import {Header} from "./components/index";
 import { AllRoutes } from "./routes/AllRoutes";
 import {ToasterWrapper} from "./Utils/ToastWrapper";
+import {useLocation} from "react-router-dom";
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <Header/>
+     {location.pathname !== "/" && <Header/>
+    }
       <ToasterWrapper/>
       <AllRoutes/>
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,11 +4,10 @@ const Home=()=>{
     return(
         <section className="home-section">
         <div className="home-text">
-        <h4 className="h1">Want to learn how to make DIY Decor Items for your home ?</h4>
-        <p className="center-text top-gutter-sm">Learn from best</p>
+        <h4 className="h1">Learn to make DIY Decor Items with <span className="highlight-header">DecorArt !!</span> </h4>
         <div className="home-btn-container flex flex-center">
         <Link to="/explore" className="btn btn-solid-primary btn-sm btn-rounded-5 btn-color">Explore</Link>
-       <Link to="/login" className="btn btn-outline-primary btn-sm btn-rounded-5">Login</Link>
+       <Link to="/login" className="btn btn-outline-primary btn-sm btn-rounded-5 login-link center-text">Login</Link>
        </div>
         </div>
         </section>
